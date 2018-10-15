af1883fac0958b0977a29d894f38df77ee1f8177
Sreekanth kotra <skotra@asiapac.globalcsc.net>
2018-10-15 19:00:37 +0530

    New changes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
  const express = require('express');
  const mongoose = require('mongoose');
+ const passport = require('passport');
  const app = express();
+
+
+ //Passport Config
+ require('./config/passport')(passport);
+ //Load Routes
+ const auth = require('./routes/auth');
 
 app.get('/', (req,res)=>{
 res.send('it works');
 });
+
+//Use Routes
+app.use('/auth', auth);
+
  const port = process.env.PORT || 5000;
 
  app.listen(port, ()=>{
