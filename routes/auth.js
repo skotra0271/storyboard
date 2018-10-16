b50464b3fdd157423e17742b8308e8c9572eb9b3
sreekanth kotra <sreekanth.kotra@hotmail.com>
2018-10-16 07:56:33 +0530

    auth handled

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,9 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-router.get('/google', passport.authenticate('google', {scope:['profile','email']}));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/home' }),  (req, res)=> {
+    res.redirect('/dashboard');
+});
 module.exports = router;
